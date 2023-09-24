e942ba07991ec92bb792c8f6cb16420421bec8af
MJAustinDev <mjaustin2022@gmail.com>
2023-09-24 17:57:34 +0100

    Phase 2: Demo level function matches new changes

--- GameControl/Levels/levelDemo.cpp
+++ GameControl/Levels/levelDemo.cpp
@@ -37,9 +37,8 @@ namespace {
 
 namespace level {
 
-void setUpDemoLevel(b2World &p_world, std::list<std::unique_ptr<entity::DynamicEntity>> &p_blocks,
-                    std::list<std::unique_ptr<entity::StaticEntity>> &p_terrain,
-                    std::list<std::unique_ptr<entity::TargetEntity>> &p_enemies) {
+void setUpDemoLevel(b2World &p_world, std::list<std::unique_ptr<entity::DynamicEntity>> &p_dynamic,
+                    std::list<std::unique_ptr<entity::StaticEntity>> &p_static) {
 
     const std::vector<b2Vec2> kGroundShape = {
         b2Vec2(0.0f, 0.0f),
@@ -48,9 +47,9 @@ void setUpDemoLevel(b2World &p_world, std::list<std::unique_ptr<entity::DynamicE
         b2Vec2(30.0f, 0.0f)
     };
 
-    p_terrain.push_back(std::make_unique<entity::StaticEntity>(p_world, b2Vec2(-5.0f, 0.0f), kGroundShape));
-    p_terrain.push_back(std::make_unique<entity::StaticEntity>(p_world, b2Vec2(25.0f, 0.0f), kGroundShape));
-    p_terrain.push_back(std::make_unique<entity::StaticEntity>(p_world, b2Vec2(65.0f, 0.0f), kGroundShape));
+    p_static.push_back(std::make_unique<entity::StaticEntity>(p_world, b2Vec2(-5.0f, 0.0f), kGroundShape));
+    p_static.push_back(std::make_unique<entity::StaticEntity>(p_world, b2Vec2(25.0f, 0.0f), kGroundShape));
+    p_static.push_back(std::make_unique<entity::StaticEntity>(p_world, b2Vec2(65.0f, 0.0f), kGroundShape));
 
     const std::vector<b2Vec2> kVerticalBlock = {
         b2Vec2(0.0f, 0.0f),
@@ -66,11 +65,11 @@ void setUpDemoLevel(b2World &p_world, std::list<std::unique_ptr<entity::DynamicE
         b2Vec2(0.0f, 1.0f)
     };
 
-    auto placeTower = [&p_world, &p_blocks, &kVerticalBlock](b2Vec2 p_position) {
+    auto placeTower = [&p_world, &p_dynamic, &kVerticalBlock](b2Vec2 p_position) {
         b2Vec2 position = p_position;
         for (int j=0; j<2; j++) {
             for (int i=0; i<3; i++) {
-                addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, position, kVerticalBlock));
+                addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, position, kVerticalBlock));
                 position.y += 5.0f;
             }
             position = p_position;
@@ -79,32 +78,32 @@ void setUpDemoLevel(b2World &p_world, std::list<std::unique_ptr<entity::DynamicE
     };
 
     placeTower(b2Vec2(52.0f, 0.0f));
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(53.0f, 15.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(53.0f, 15.5f), 0.5f));
 
     placeTower(b2Vec2(67.0f, 0.0f));
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(68.0f, 15.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(68.0f, 15.5f), 0.5f));
 
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 0.0f), kVerticalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 5.0f), kHorizontalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 6.0f), kHorizontalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 7.0f), kHorizontalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(73.5f, 0.0f), kVerticalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(74.0f, 5.0f), kHorizontalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(74.0f, 6.0f), kHorizontalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(74.0f, 7.0f), kHorizontalBlock));
-    addDynamic(p_blocks, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(78.0f, 0.0f), kVerticalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 0.0f), kVerticalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 5.0f), kHorizontalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 6.0f), kHorizontalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(69.0f, 7.0f), kHorizontalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(73.5f, 0.0f), kVerticalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(74.0f, 5.0f), kHorizontalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(74.0f, 6.0f), kHorizontalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(74.0f, 7.0f), kHorizontalBlock));
+    addDynamic(p_dynamic, std::make_unique<entity::BlockEntity>(p_world, b2Vec2(78.0f, 0.0f), kVerticalBlock));
 
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(72.0f, 0.5f), 0.5f));
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(76.0f, 0.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(72.0f, 0.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(76.0f, 0.5f), 0.5f));
 
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(71.0f, 8.5f), 0.5f));
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(74.0f, 8.5f), 0.5f));
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(77.0f, 8.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(71.0f, 8.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(74.0f, 8.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(77.0f, 8.5f), 0.5f));
 
     placeTower(b2Vec2(79.0f, 0.0f));
-    addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(80.0f, 15.5f), 0.5f));
+    addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(80.0f, 15.5f), 0.5f));
 
-    // addDynamic(p_blocks, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(73.5f, 1500.0f), 4.0f)); // Meteor of death... TODO remove later
+    // addDynamic(p_dynamic, std::make_unique<entity::TargetEntity>(p_world, b2Vec2(73.5f, 1500.0f), 4.0f)); // Meteor of death... TODO remove later
 
 }
 
