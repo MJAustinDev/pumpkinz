f717a23421ecbc6860c22385572d4ca81d0599bd
MJAustinDev <mjaustin2022@gmail.com>
2024-03-10 16:57:43 +0000

    Phase 4: Pumpkin patch level 3

--- GameControl/Missions/PumpkinPatch/Missions_1_15/pumpkinPatchMission_3.cpp
+++ GameControl/Missions/PumpkinPatch/Missions_1_15/pumpkinPatchMission_3.cpp
@@ -24,19 +24,69 @@
 
 #include "mission.h"
 
+namespace {
+
+using StaticEntity = shadow_pumpkin_caster::entity::StaticEntity;
+using Pumpkin = shadow_pumpkin_caster::entity::enemy::Pumpkin;
+using BlockEntity = shadow_pumpkin_caster::entity::BlockEntity;
+
+}; // end of namespace
+
 namespace shadow_pumpkin_caster::missions::pumpkin_patch {
 
-using StaticEntity = entity::StaticEntity;
-
+/**
+ * Level contains a tower with 3 pumpkins on each level
+ */
 void setUpMission_3(MissionEntities_t &p_entities, b2World &p_world) {
     const std::vector<b2Vec2> kGroundShape = {
         b2Vec2(0.0f, 0.0f),
         b2Vec2(0.0f, -5.0f),
-        b2Vec2(150.0f, -5.0f),
-        b2Vec2(150.0f, 0.0f)
+        b2Vec2(15.0f, -5.0f),
+        b2Vec2(15.0f, 0.0f)
     };
     auto ground = std::make_unique<StaticEntity>(p_world, b2Vec2(-5.0f, 0.0f), kGroundShape);
     p_entities.staticGround.push_back(std::move(ground));
+
+    ground = std::make_unique<StaticEntity>(p_world, b2Vec2(75.0f, 0.0f), kGroundShape);
+    p_entities.staticGround.push_back(std::move(ground));
+
+    auto placeTower = [&p_entities, &p_world](float p_positionY) {
+        const std::vector<b2Vec2> kWallShape = {
+            b2Vec2(0.0f, 8.0f),
+            b2Vec2(0.0f, 0.0f),
+            b2Vec2(2.0f, 0.0f),
+            b2Vec2(2.0f, 8.0f)
+        };
+        const std::vector<b2Vec2> kRoofShape = {
+            b2Vec2(0.0f, 2.0f),
+            b2Vec2(0.0f, 0.0f),
+            b2Vec2(13.0f, 0.0f),
+            b2Vec2(13.0f, 2.0f)
+        };
+
+        auto block = std::make_shared<BlockEntity>(p_world, b2Vec2(76.0f, p_positionY), kWallShape);
+        p_entities.destructableBlocks.push_back(std::move(block));
+
+        block = std::make_shared<BlockEntity>(p_world, b2Vec2(87.0f, p_positionY), kWallShape);
+        p_entities.destructableBlocks.push_back(std::move(block));
+
+        block = std::make_shared<BlockEntity>(p_world, b2Vec2(76.0f, 8.0f + p_positionY),
+                                              kRoofShape);
+        p_entities.destructableBlocks.push_back(std::move(block));
+
+        auto pumpkin = std::make_shared<Pumpkin>(p_world, b2Vec2(79.5f, 0.5f + p_positionY), 1.0f);
+        p_entities.pumpkins.push_back(std::move(pumpkin));
+
+        pumpkin = std::make_shared<Pumpkin>(p_world, b2Vec2(85.5f, 0.5f + p_positionY), 1.0f);
+        p_entities.pumpkins.push_back(std::move(pumpkin));
+
+        pumpkin = std::make_shared<Pumpkin>(p_world, b2Vec2(82.5f, 1.5f + p_positionY), 1.5f);
+        p_entities.pumpkins.push_back(std::move(pumpkin));
+    };
+
+    for (int i = 0; i < 5; i++) {
+        placeTower(static_cast<float>(i) * 10.0f);
+    }
 };
 
 };
